503d9c1cf9694d78e013a6dddc6b0c6954d4a442
adinafometescu <adina.fometescu@gmail.com>
2017-08-05 13:46:03 +0300

    added module caching tutorial

---
+++ spring-tutorials/caching-tutorial/src/main/java/com/tutorials/spring/caching/CachingConfig.java
@@ -0,0 +1,9 @@
+package com.tutorials.spring.caching;
+
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableCaching
+public class CachingConfig {
+}
