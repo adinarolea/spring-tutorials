bb872e77859dc40618b5b3c977f1d2ae0db347fe
adinafometescu <adina.fometescu@gmail.com>
2017-09-10 16:55:26 +0300

    added kafka message producer

---
+++ kafka-tutorial/kafka-producer/src/main/java/com/tutorial/kafka/producer/KafkaProducer.java
@@ -0,0 +1,24 @@
+package com.tutorial.kafka.producer;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Component;
+import org.springframework.util.Assert;
+
+@Component
+public class KafkaProducer {
+
+    private static final Logger logger = LoggerFactory.getLogger(KafkaProducer.class);
+
+    @Autowired
+    private KafkaTemplate<String, String> kafkaTemplate;
+
+    public void sendMessage(String msg) {
+        Assert.notNull(msg, "Input message cannot be null");
+        logger.info("Sending message: " + msg);
+        kafkaTemplate.send("foo", msg);
+    }
+}
