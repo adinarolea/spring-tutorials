731a9cedb4959607a96ef18e0ea532c1d28ebd4d
Adina Fometescu <adina.fometescu@elan-solutions.eu>
2018-07-09 13:42:53 +0300

    renamed project name

--- spring-cloud-eureka/spring-cloud-client-consumer/src/main/java/com/tutorials/spring/cloud/client/consumer/eureka/FeignCoffee.java
+++
@@ -1,8 +0,0 @@
-package com.tutorials.spring.cloud.client.consumer.eureka;
-
-import com.tutorials.spring.cloud.client.eureka.CoffeeClient;
-import org.springframework.cloud.netflix.feign.FeignClient;
-
-@FeignClient("spring-cloud-client")
-public interface FeignCoffee extends CoffeeClient {
-}
