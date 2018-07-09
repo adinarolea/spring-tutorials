2d02e2fed8d7d11a6c3d83cc990d3f0360c60ab7
Adina Fometescu <adina.fometescu@elan-solutions.eu>
2018-07-09 13:40:03 +0300

    renamed modules and project to comply with another spring cloud tutorials

--- spring-cloud-eureka/spring-cloud-eureka-client/src/main/java/com/tutorials/spring/eureka/client/CoffeeClient.java
+++
@@ -1,8 +0,0 @@
-package com.tutorials.spring.eureka.client;
-
-import org.springframework.web.bind.annotation.RequestMapping;
-
-public interface CoffeeClient {
-    @RequestMapping("/coffee")
-    String getCoffee();
-}
--- spring-cloud-eureka/spring-cloud-eureka-feign/src/main/java/com/tutorials/spring/eureka/feign/FeignCoffee.java
+++
@@ -1,8 +0,0 @@
-package com.tutorials.spring.eureka.feign;
-
-import com.tutorials.spring.eureka.client.CoffeeClient;
-import org.springframework.cloud.netflix.feign.FeignClient;
-
-@FeignClient("spring-cloud-eureka-client")
-public interface FeignCoffee extends CoffeeClient {
-}
