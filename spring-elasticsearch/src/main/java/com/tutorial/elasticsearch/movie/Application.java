f780b37ad701edfcc25c5273eb81932ea67b8d96
adinafometescu <adina.fometescu@gmail.com>
2018-09-23 16:58:34 +0300

    updated spring boot to 2.0.5 and added docker to start elasticsearch container

--- spring-elasticsearch/src/main/java/com/tutorial/elasticsearch/movie/Application.java
+++ spring-elasticsearch/src/main/java/com/tutorial/elasticsearch/movie/Application.java
@@ -5,8 +5,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 
-@Import(ElasticsearchConfig.class)
 @SpringBootApplication
+@Import(ElasticsearchConfig.class)
 public class Application {
 
     public static void main(String[] args) {
