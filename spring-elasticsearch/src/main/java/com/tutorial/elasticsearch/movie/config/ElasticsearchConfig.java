6d6889ee2f8c0497993dbcf1c9b2832a105f2b73
adinafometescu <adina.fometescu@gmail.com>
2018-10-06 17:50:38 +0300

    updated docker compose and configuration

--- spring-elasticsearch/src/main/java/com/tutorial/elasticsearch/movie/config/ElasticsearchConfig.java
+++ spring-elasticsearch/src/main/java/com/tutorial/elasticsearch/movie/config/ElasticsearchConfig.java
@@ -1,38 +1,9 @@
 package com.tutorial.elasticsearch.movie.config;
 
-import org.elasticsearch.client.Client;
-import org.elasticsearch.client.transport.TransportClient;
-import org.elasticsearch.common.settings.Settings;
-import org.elasticsearch.common.transport.InetSocketTransportAddress;
-import org.elasticsearch.transport.client.PreBuiltTransportClient;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
-import org.springframework.data.elasticsearch.core.ElasticsearchTemplate;
 import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
-
-import java.net.InetAddress;
 
 @Configuration
 @EnableElasticsearchRepositories
 public class ElasticsearchConfig {
-
-    @Bean
-    public Client client() throws Exception {
-
-        Settings esSettings = Settings.builder()
-                .put("cluster.name", "elasticsearch")
-                .put("client.transport.sniff", false)
-                .build();
-
-
-        TransportClient client = new PreBuiltTransportClient(esSettings)
-                .addTransportAddress(new InetSocketTransportAddress(InetAddress.getByName("127.0.0.1"), 9300));
-        return client;
-    }
-
-    @Bean
-    public ElasticsearchOperations elasticsearchTemplate() throws Exception {
-        return new ElasticsearchTemplate(client());
-    }
 }
